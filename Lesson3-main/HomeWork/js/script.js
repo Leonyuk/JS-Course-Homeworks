8bb19662fb0eb73d89723f0057247f81055ce307
Viktor <leonyuk48@gmail.com>
2026-03-16 17:53:44 +0200

    виконана домашня робота 3

--- Lesson3-main/HomeWork/js/script.js
+++ Lesson3-main/HomeWork/js/script.js
@@ -2,15 +2,30 @@
 // У вас є масив об’єктів fruts, і в кожному з них є name
 // Напишіть код, який перетворює їх в масив імен.
 
+// let fruts = [
+//   { id: 0, name: "Apple" },
+//   { id: 1, name: "Tomat" },
+//   { id: 2, name: "Cherry" },
+//   { id: 3, name: "Orange" },
+// ];
+// let names = fruts.map((item) => item.name);
+// console.log(names);
+
 // const fruts = [
 //   { id: 0, name: "Apple" },
 //   { id: 1, name: "Tomat" },
 //   { id: 2, name: "Cherry" },
 //   { id: 3, name: "Orange" },
 // ];
+// let names = [fruts.map((item) => item.name)];
+// console.log(names);
 
 //  -- 2 --
 //Виведіть парні числа від 2 до 10, використовуючи цикл for.
+
+// for (let i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
 
 //  -- 3 --
 //Замініть цикл "for" на "while"
@@ -18,11 +33,22 @@
 //     console.lpg( `цифра ${i}!` );
 //   }
 
+// let i = 0;
+// while (i < 5) {
+//   console.log(`цифра ${i}!`);
+//     i++;
+// }
+
 //  -- 4 --
 //Напишіть цикл, який пропонує prompt ввести число більше за 100.
 //Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
 //Цикл повинен запитувати число доти, доки відвідувач не введе число,
 // більше за 100, або не скасує ввід/введе порожній рядок.
+
+// let number = prompt("Введіть число більше 100");
+// while (number <= 100 && number !== null && number !== "") {
+//     number = prompt("Введіть число більше 100");
+// }
 
 //  -- 5 --
 // Вирахуйте середній вік
@@ -33,3 +59,8 @@
 //   { age: 10, name: "Юля" },
 //   { age: 20, name: "Катя" },
 // ];
+// let totalAge = girls.reduce((sum, girl) => sum + girl.age, 0);
+// let averageAge = totalAge / girls.length;
+// console.log(averageAge);
+
+//середній вік 20.5
