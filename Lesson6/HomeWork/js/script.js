269d1baeed14d5f3aeaf6a22b3517a8c14110376
Viktor <leonyuk48@gmail.com>
2026-03-24 13:03:31 +0200

    Створив папку Lesson6 та виконав домашнє завдання № 6

---
+++ Lesson6/HomeWork/js/script.js
@@ -0,0 +1,24 @@
+// class Abonent {
+//   constructor(name, surname, phone) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.phone = phone;
+//   }
+
+//   set(name, surname, phone) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.phone = phone;
+//   }
+
+//   get() {
+//     return `Абонент: ${this.name} ${this.surname}. Телефон: ${this.phone}`;
+//   }
+// }
+
+// const abonent1 = new Abonent("Иван", "Сидоренко", "+380503541734");
+// console.log(abonent1.get());
+// const abonent2 = new Abonent("Ирина", "Ткаченко", "+380992056487");
+// console.log(abonent2.get());
+// const abonent3 = new Abonent("Марк", "Рабинович", "+38672834577");
+// console.log(abonent3.get());
