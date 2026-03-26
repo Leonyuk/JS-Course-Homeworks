8f20c2e6734b0ef347093293350a9df699035d67
Viktor <leonyuk48@gmail.com>
2026-03-26 16:58:12 +0200

    Виконане домашнє завдання № 7

---
+++ Lesson7/HomeWork/js/script.js
@@ -0,0 +1,20 @@
+// function Calculator() {
+//   this.read = function () {
+//     this.a = +prompt("Введіть a:", 0);
+//     this.b = +prompt("Введіть b:", 0);
+//   };
+
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+
+//   this.mul = function () {
+//     return this.a * this.b;
+//   };
+// }
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert(calculator.sum());
+// alert(calculator.mul());
---
+++ Lesson7/Lesson/js/script.js
@@ -0,0 +1,8 @@
+//2 Arrow function
+
+// const sum = (a, b) => a + b;
+// const diff = (a, b) => {
+//   return a - b;
+// };
+// console.log("Arrow Function Add Result: " + sum(5, 3));
+
