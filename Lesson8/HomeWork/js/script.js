bd695bd159d40a8b54cae687c4b63a757bc746b7
Viktor <leonyuk48@gmail.com>
2026-03-27 13:33:56 +0200

    Додав файли уроку 8

---
+++ Lesson8/HomeWork/js/script.js
@@ -0,0 +1,9 @@
+
+// const widgetElement = document.querySelector('[data-widget-name]');
+
+// if (widgetElement) {
+//     const value = widgetElement.getAttribute('data-widget-name');
+//     console.log(value);
+// }
+
+// console.log(widgetElement);
